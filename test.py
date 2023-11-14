c9e0bc48166662757b4ff0ce8c2372372157fae6
kovati <125868538+kovati@users.noreply.github.com>
2023-11-14 14:35:12 +0100

    playing with collections in test.py

--- test.py
+++ test.py
@@ -9,3 +9,15 @@ for i in reversed(range(len(szoveg))):
 print(ujszoveg)
 import math
 print(math.log2(16))
+valami = {24,45,33}
+print(type(valami))
+print(valami.union({23,45,78,89}))
+valami = {24:34,45:56,67:78}
+print(type(valami))
+print(valami.keys())
+valami = [1234,1234,1234]
+print(type(valami))
+print(valami[2])
+valami = (1,2,3,4,6)
+print(type(valami))
+print(valami[2])
